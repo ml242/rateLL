711b56af0f9004961345558b8585adda11e2ee09
Matt Lucas <mattlucas@gmail.com>
2013-11-19 11:47:18 -0500

    getting started with kat and eddie

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RateLL::Application.config.secret_token = '0cb33a4860dbf00aa7244ea5ba5b80e3f7eda23108eebd1eba7ebc93046f833007ac171174baa52ebd685408a8b62373fb592e0656cd885ac996e588db4e7989'
